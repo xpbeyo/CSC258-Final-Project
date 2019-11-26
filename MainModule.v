e54d003235cfa1f97169a1af11f6138c3e0cadd8
Samuel <samuelyixp@outlook.com>
2019-11-25 23:09:06 -0500

    changed grids to 2d arrary

--- MainModule.v
+++ MainModule.v
@@ -88,18 +88,18 @@ module MainModule(SW, KEY, LEDR, CLOCK_50, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
 	 
     output [2:0] LEDR;
     output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-    wire [17:0] grid;
+    wire [1:0] grid [2:0][2:0];;
     MileStoneOne mso(
         .resetn(KEY[1]),
         .clk(CLOCK_50),
         .confirm(KEY[2]),
         .address(SW[3:0]),
-        .grid(grid[17:0]),
+        .grid(grid[2:0][2:0]]),
         .winner(LEDR[1:0]),
 		.end_signal(LEDR[2])
     );
 
-    GridDecoder gd(grid[17:0], 
+    GridDecoder gd(grid[2:0][2:0], 
     HEX5[6:0], 
     HEX4[6:0], 
     HEX3[6:0], 
@@ -115,12 +115,12 @@ module MileStoneOne(resetn, clk, confirm, address, grid, winner, end_signal);
     input clk;
     input confirm;
     output [1:0] winner;
-    output [17:0] grid;
+    output [1:0] grid [2:0][2:0];;
     input [3:0] address;
     wire ld;
 	wire [1:0] value;
     output end_signal;
-    WinCondition wc(.pos(grid[17:0]), 
+    WinCondition wc(.grid(grid[2:0][2:0]), 
     .winner(winner[1:0]), 
     .end_signal(end_signal));
 
@@ -136,7 +136,7 @@ module MileStoneOne(resetn, clk, confirm, address, grid, winner, end_signal);
     .value(value), 
     .ld(ld), 
     .address(address),
-	 .grid(grid[17:0]),
+	 .grid(grid[2:0][2:0]),
     .clk(clk));
 
 
@@ -276,7 +276,7 @@ endmodule
 //
 //    end
 //
-//    always @(posedge clk) begin
+//    always @(gridedge clk) begin
 //        if (~resetn)
 //            current_grid <= 4'b0000;
 //        else
@@ -325,7 +325,7 @@ module FSMControl(clk, resetn, confirm, end_sig, ld, value);
                         next_state = load_two;
                     else
                         next_state = load_two_idle;
-					  end
+					end
             end
             load_two: begin
                 if (end_sig)
@@ -362,10 +362,10 @@ module FSMControl(clk, resetn, confirm, end_sig, ld, value);
                 value = 2'd2;
                 ld = 1'b1;
             end
-			endcase
+		endcase
 
     end
-	always @(posedge clk) begin
+	always @(gridedge clk) begin
         if (~resetn) begin
             current_state <= load_one_idle;
         end
@@ -380,74 +380,83 @@ module DataPathGrid(resetn, value, ld, address, grid, draw_grid, clk);
     input [1:0] value;
     input ld;
     input [3:0] address;
-    output reg [17:0] grid;
+    output reg [1:0] grid [2:0][2:0];
     output reg [2:0] draw_grid [0:159][0:119];
     input clk;
-    always @(posedge clk) begin
+    always @(gridedge clk) begin
         if (~resetn) begin
-            grid[17:16] = 2'd0;
-            grid[15:14] = 2'd0;
-            grid[13:12] = 2'd0;
-            grid[11:10] = 2'd0;
-            grid[9:8] = 2'd0;
-            grid[7:6] = 2'd0;
-            grid[5:4] = 2'd0;
-            grid[3:2] = 2'd0;
-            grid[1:0] = 2'd0;
+            grid[0][0] = 2'd0;
+            grid[0][1] = 2'd0;
+            grid[0][2] = 2'd0;
+            grid[1][0] = 2'd0;
+            grid[1][1] = 2'd0;
+            grid[1][2] = 2'd0;
+            grid[2][0] = 2'd0;
+            grid[2][1] = 2'd0;
+            grid[2][2] = 2'd0;
 		end
         else if (ld) begin
             if (address == 4'b0000)
-                grid[17:16] <= value;
+                grid[0][0] <= value;
             else if (address == 4'b0001)
-                grid[15:14] <= value;
+                grid[0][1] <= value;
             else if (address == 4'b0010)
-                grid[13:12] <= value;
+                grid[0][2] <= value;
             else if (address == 4'b0011)
-                grid[11:10] <= value;
+                grid[1][0] <= value;
             else if (address == 4'b0100)
-                grid[9:8] <= value;
+                grid[1][1] <= value;
             else if (address == 4'b0101)
-                grid[7:6] <= value;
+                grid[1][2] <= value;
             else if (address == 4'b0110)
-                grid[5:4] <= value;
+                grid[2][0] <= value;
             else if (address == 4'b0111)
-                grid[3:2] <= value;
+                grid[2][1] <= value;
             else if (address == 4'b1000)
-                grid[1:0] <= value; 
+                grid[2][2] <= value; 
+        end
+        for (y = 0; y < 160; y = y + 1) begin
+            for (x = 0; x < 120; x = x + 1) begin
+                if (grid[y][x] == 2'd1) begin
+                    
+
+                end
+
+            end
         end
     end
 endmodule
 
-module WinCondition(pos, winner, end_signal);
+module WinCondition(grid, winner, end_signal);
     // player one has chess O, player two has chess X.
-    input [17:0] pos;  // 2'd1 represents O, 2'd2 represents X, 2'd0 represents empty.
+    input [1:0] grid [2:0][2:0];  // 2'd1 represents O, 2'd2 represents X, 2'd0 represents empty.
     output reg [1:0] winner;  // who win the game, 2'b00:undetermine, 2'b01: first player, 2'b10: second player, 2'b11: draw.
     output reg end_signal;  // 0 if not end, 1 if end.
     wire check_full;  // 0 if not full, 1 if full.
 
-    SpaceFull space_detector(.pos(pos[17:0]), .full(check_full));
+    SpaceFull space_detector(.grid(grid[17:0]), .full(check_full));
 	always @(*) begin
-		if ((pos[1:0] == 2'd1 && pos[3:2] == 2'd1 && pos[5:4] == 2'd1) ||  // horizontal
-			  (pos[7:6] == 2'd1 && pos[9:8] == 2'd1 && pos[11:10] == 2'd1) ||
-			  (pos[13:12] == 2'd1 && pos[15:14] == 2'd1 && pos[17:16] == 2'd1) ||
-			  (pos[1:0] == 2'd1 && pos[7:6] == 2'd1 && pos[13:12] == 2'd1) ||  // vertical
-			  (pos[3:2] == 2'd1 && pos[9:8] == 2'd1 && pos[15:14] == 2'd1) ||
-			  (pos[5:4] == 2'd1 && pos[11:10] == 2'd1 && pos[17:16] == 2'd1) ||
-			  (pos[1:0] == 2'd1 && pos[9:8] == 2'd1 && pos[17:16] == 2'd1) ||  // diagonal
-			  (pos[5:4] == 2'd1 && pos[9:8] == 2'd1 && pos[13:12] == 2'd1)) begin
+		if ((grid[2][2] == 2'd1 && grid[2][1] == 2'd1 && grid[2][0] == 2'd1) ||  // horizontal
+			  (grid[1][2] == 2'd1 && grid[1][1] == 2'd1 && grid[1][0] == 2'd1) ||
+			  (grid[0][2] == 2'd1 && grid[0][1] == 2'd1 && grid[0][0] == 2'd1) ||
+			  (grid[2][2] == 2'd1 && grid[1][2] == 2'd1 && grid[0][2] == 2'd1) ||  // vertical
+			  (grid[2][1] == 2'd1 && grid[1][1] == 2'd1 && grid[0][1] == 2'd1) ||
+			  (grid[2][0] == 2'd1 && grid[1][0] == 2'd1 && grid[0][0] == 2'd1) ||
+			  (grid[2][2] == 2'd1 && grid[1][1] == 2'd1 && grid[0][0] == 2'd1) ||  // diagonal
+			  (grid[2][0] == 2'd1 && grid[1][1] == 2'd1 && grid[0][2] == 2'd1)) begin
 					winner = 2'b01;
 					end_signal = 1'b1;
 			end
 	
 
-		else if ((pos[1:0] == 2'd2 && pos[3:2] == 2'd2 && pos[5:4] == 2'd2) ||  // horizontal
-			  (pos[7:6] == 2'd2 && pos[9:8] == 2'd2 && pos[11:10] == 2'd2) ||
-			  (pos[13:12] == 2'd2 && pos[15:14] == 2'd2 && pos[17:16] == 2'd2) ||
-			  (pos[1:0] == 2'd2 && pos[7:6] == 2'd2 && pos[13:12] == 2'd2) ||  // vertical
-			  (pos[3:2] == 2'd2 && pos[9:8] == 2'd2 && pos[15:14] == 2'd2) ||
-			  (pos[5:4] == 2'd2 && pos[11:10] == 2'd2 && pos[17:16] == 2'd2) ||
-			  (pos[1:0] == 2'd2 && pos[9:8] == 2'd2 && pos[17:16] == 2'd2) ||  // diagonal
-			  (pos[5:4] == 2'd2 && pos[9:8] == 2'd2 && pos[13:12] == 2'd2)) begin
+		else if ((grid[2][2] == 2'd2 && grid[2][1] == 2'd2 && grid[2][0] == 2'd2) ||  // horizontal
+			  (grid[1][2] == 2'd2 && grid[1][1] == 2'd2 && grid[1][0] == 2'd2) ||
+			  (grid[0][2] == 2'd2 && grid[0][1] == 2'd2 && grid[0][0] == 2'd2) ||
+			  (grid[2][2] == 2'd2 && grid[1][2] == 2'd2 && grid[0][2] == 2'd2) ||  // vertical
+			  (grid[2][1] == 2'd2 && grid[1][1] == 2'd2 && grid[0][1] == 2'd2) ||
+			  (grid[2][0] == 2'd2 && grid[1][0] == 2'd2 && grid[0][0] == 2'd2) ||
+			  (grid[2][2] == 2'd2 && grid[1][1] == 2'd2 && grid[0][0] == 2'd2) ||  // diagonal
+			  (grid[2][0] == 2'd2 && grid[1][1] == 2'd2 && grid[0][2] == 2'd2)) begin
 					winner = 2'b10;
 					end_signal = 1'b1;
 			  end
@@ -463,20 +472,20 @@ module WinCondition(pos, winner, end_signal);
 	end
 endmodule
 
-module SpaceFull(pos, full);
-    input [17:0] pos;  // 2'd1 represents O, 2'd2 represents X, 2'd0 represents empty.
+module SpaceFull(grid, full);
+    input [1:0] grid [2:0][2:0];  // 2'd1 represents O, 2'd2 represents X, 2'd0 represents empty.
     output reg full;  // 0 represents not full, 1 represents full.
 	always @(*) begin
-        if (pos[17:16] != 2'd0 && pos[15:14] != 2'd0 && pos[13:12] != 2'd0 &&
-            pos[11:10] != 2'd0 && pos[9:8] != 2'd0 && pos[7:6] != 2'd0 &&
-            pos[5:4] != 2'd0 && pos[3:2] != 2'd0 && pos[1:0] != 2'd0)
+        if (grid[0][0] != 2'd0 && grid[0][1] != 2'd0 && grid[0][2] != 2'd0 &&
+            grid[1][0] != 2'd0 && grid[1][1] != 2'd0 && grid[1][2] != 2'd0 &&
+            grid[2][0] != 2'd0 && grid[2][1] != 2'd0 && grid[2][2] != 2'd0)
                 full = 1'b1;
         else full = 1'b0;
 	end
 endmodule
 
 module GridDecoder(grid, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
-    input [17:0] grid;
+    input [1:0] grid [2:0][2:0];
     output reg [6:0] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
 	always @(*) begin
 		HEX5 = 7'b1111111;
@@ -485,41 +494,41 @@ module GridDecoder(grid, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
 		HEX2 = 7'b1111111;
 		HEX1 = 7'b1111111;
 		HEX0 = 7'b1111111;
-        if (grid[17:16] == 2'd1)
+        if (grid[0][0] == 2'd1)
             HEX5[0] = 1'b0;
-        else if (grid[17:16] == 2'd2) 
+        else if (grid[0][0] == 2'd2) 
             HEX2[0] = 1'b0;
-        if (grid[15:14] == 2'd1)
+        if (grid[0][1] == 2'd1)
             HEX4[0] = 1'b0;
-        else if (grid[15:14] == 2'd2) 
+        else if (grid[0][1] == 2'd2) 
             HEX1[0] = 1'b0;
-        if (grid[13:12] == 2'd1)
+        if (grid[0][2] == 2'd1)
             HEX3[0] = 1'b0;
-        else if (grid[13:12] == 2'd2) 
+        else if (grid[0][2] == 2'd2) 
             HEX0[0] = 1'b0;
-        if (grid[11:10] == 2'd1)
+        if (grid[1][0] == 2'd1)
             HEX5[6] = 1'b0;
-        else if (grid[11:10] == 2'd2) 
+        else if (grid[1][0] == 2'd2) 
             HEX2[6] = 1'b0;
-        if (grid[9:8] == 2'd1)
+        if (grid[1][1] == 2'd1)
             HEX4[6] = 1'b0;
-        else if (grid[9:8] == 2'd2) 
+        else if (grid[1][1] == 2'd2) 
             HEX1[6] = 1'b0;
-        if (grid[7:6] == 2'd1)
+        if (grid[1][2] == 2'd1)
             HEX3[6] = 1'b0;
-        else if (grid[7:6] == 2'd2) 
+        else if (grid[1][2] == 2'd2) 
             HEX0[6] = 1'b0;
-        if (grid[5:4] == 2'd1)
+        if (grid[2][0] == 2'd1)
             HEX5[3] = 1'b0;
-        else if (grid[5:4] == 2'd2) 
+        else if (grid[2][0] == 2'd2) 
             HEX2[3] = 1'b0;
-        if (grid[3:2] == 2'd1)
+        if (grid[2][1] == 2'd1)
             HEX4[3] = 1'b0;
-        else if (grid[3:2] == 2'd2) 
+        else if (grid[2][1] == 2'd2) 
             HEX1[3] = 1'b0;
-        if (grid[1:0] == 2'd1)
+        if (grid[2][2] == 2'd1)
             HEX3[3] = 1'b0;
-        else if (grid[1:0] == 2'd2) 
+        else if (grid[2][2] == 2'd2) 
             HEX0[3] = 1'b0;
 	end
 endmodule
