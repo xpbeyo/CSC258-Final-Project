734b42efd5d98bcb07de76c96e7bb40d3b5827dd
Samuel <samuelyixp@outlook.com>
2019-11-25 22:20:07 -0500

    Refactored the grid to a 2D array.

--- MainModule.v
+++ MainModule.v
@@ -1,3 +1,82 @@
+// // Part 2 skeleton
+
+// module part2
+// 	(
+// 		CLOCK_50,						//	On Board 50 MHz
+// 		// Your inputs and outputs here
+//         KEY,
+//         SW,
+// 		// The ports below are for the VGA output.  Do not change.
+// 		VGA_CLK,   						//	VGA Clock
+// 		VGA_HS,							//	VGA H_SYNC
+// 		VGA_VS,							//	VGA V_SYNC
+// 		VGA_BLANK_N,						//	VGA BLANK
+// 		VGA_SYNC_N,						//	VGA SYNC
+// 		VGA_R,   						//	VGA Red[9:0]
+// 		VGA_G,	 						//	VGA Green[9:0]
+// 		VGA_B   						//	VGA Blue[9:0]
+// 	);
+
+// 	input			CLOCK_50;				//	50 MHz
+// 	input   [9:0]   SW;
+// 	input   [3:0]   KEY;
+
+// 	// Declare your inputs and outputs here
+// 	// Do not change the following outputs
+// 	output			VGA_CLK;   				//	VGA Clock
+// 	output			VGA_HS;					//	VGA H_SYNC
+// 	output			VGA_VS;					//	VGA V_SYNC
+// 	output			VGA_BLANK_N;				//	VGA BLANK
+// 	output			VGA_SYNC_N;				//	VGA SYNC
+// 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+// 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+// 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+// 	wire resetn;
+// 	assign resetn = KEY[0];
+	
+// 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+// 	wire [2:0] colour;
+// 	wire [7:0] x;
+// 	wire [6:0] y;
+// 	wire writeEn;
+
+// 	// Create an Instance of a VGA controller - there can be only one!
+// 	// Define the number of colours as well as the initial background
+// 	// image file (.MIF) for the controller.
+// 	vga_adapter VGA(
+// 			.resetn(resetn),
+// 			.clock(CLOCK_50),
+// 			.colour(colour),
+// 			.x(x),
+// 			.y(y),
+// 			.plot(writeEn),
+// 			/* Signals for the DAC to drive the monitor. */
+// 			.VGA_R(VGA_R),
+// 			.VGA_G(VGA_G),
+// 			.VGA_B(VGA_B),
+// 			.VGA_HS(VGA_HS),
+// 			.VGA_VS(VGA_VS),
+// 			.VGA_BLANK(VGA_BLANK_N),
+// 			.VGA_SYNC(VGA_SYNC_N),
+// 			.VGA_CLK(VGA_CLK));
+// 		defparam VGA.RESOLUTION = "160x120";
+// 		defparam VGA.MONOCHROME = "FALSE";
+// 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+// 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+			
+// 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+// 	// for the VGA controller, in addition to any other functionality your design may require.
+    
+//     // Instansiate datapath
+// 	// datapath d0(...);
+
+//     // Instansiate FSM control
+//     // control c0(...);
+    
+// endmodule
+
+
 module MainModule(SW, KEY, LEDR, CLOCK_50, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
     // SW[3:0] that specifies the address of intended move
     input [3:0] SW;
@@ -17,7 +96,7 @@ module MainModule(SW, KEY, LEDR, CLOCK_50, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
         .address(SW[3:0]),
         .grid(grid[17:0]),
         .winner(LEDR[1:0]),
-		  .end_signal(LEDR[2])
+		.end_signal(LEDR[2])
     );
 
     GridDecoder gd(grid[17:0], 
@@ -225,8 +304,7 @@ module FSMControl(clk, resetn, confirm, end_sig, ld, value);
                         next_state = load_one;
                     else
                         next_state = load_one_idle;
-					 end
-
+					end
             end
             load_one: begin
                 if (end_sig)
@@ -291,31 +369,32 @@ module FSMControl(clk, resetn, confirm, end_sig, ld, value);
         if (~resetn) begin
             current_state <= load_one_idle;
         end
-            else
-                    current_state <= next_state;
+        else
+            current_state <= next_state;
     end
 endmodule
 
-module DataPathGrid(resetn, value, ld, address, grid, clk);
+module DataPathGrid(resetn, value, ld, address, grid, draw_grid, clk);
     // active low reset
     input resetn;
     input [1:0] value;
     input ld;
     input [3:0] address;
     output reg [17:0] grid;
+    output reg [2:0] draw_grid [0:159][0:119];
     input clk;
     always @(posedge clk) begin
         if (~resetn) begin
             grid[17:16] = 2'd0;
-				grid[15:14] = 2'd0;
-				grid[13:12] = 2'd0;
-				grid[11:10] = 2'd0;
-				grid[9:8] = 2'd0;
-				grid[7:6] = 2'd0;
-				grid[5:4] = 2'd0;
-				grid[3:2] = 2'd0;
-				grid[1:0] = 2'd0;
-			end
+            grid[15:14] = 2'd0;
+            grid[13:12] = 2'd0;
+            grid[11:10] = 2'd0;
+            grid[9:8] = 2'd0;
+            grid[7:6] = 2'd0;
+            grid[5:4] = 2'd0;
+            grid[3:2] = 2'd0;
+            grid[1:0] = 2'd0;
+		end
         else if (ld) begin
             if (address == 4'b0000)
                 grid[17:16] <= value;
@@ -335,7 +414,7 @@ module DataPathGrid(resetn, value, ld, address, grid, clk);
                 grid[3:2] <= value;
             else if (address == 4'b1000)
                 grid[1:0] <= value; 
-        end                           
+        end
     end
 endmodule
 
