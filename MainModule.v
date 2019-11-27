d88bc2b5120236655f5b7fefa91a9eb4d6ef58e5
Samuel <samuelyixp@outlook.com>
2019-11-26 20:09:23 -0500

    Changed MainModule to VGA adapter

--- MainModule.v
+++ MainModule.v
@@ -1,122 +1,111 @@
-// // Part 2 skeleton
+module MainModule
+	(
+		CLOCK_50,						//	On Board 50 MHz
+		// Your inputs and outputs here
+        KEY,
+        SW,
+        LEDR,
+		// The ports below are for the VGA output.  Do not change.
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
+	);
 
-// module part2
-// 	(
-// 		CLOCK_50,						//	On Board 50 MHz
-// 		// Your inputs and outputs here
-//         KEY,
-//         SW,
-// 		// The ports below are for the VGA output.  Do not change.
-// 		VGA_CLK,   						//	VGA Clock
-// 		VGA_HS,							//	VGA H_SYNC
-// 		VGA_VS,							//	VGA V_SYNC
-// 		VGA_BLANK_N,						//	VGA BLANK
-// 		VGA_SYNC_N,						//	VGA SYNC
-// 		VGA_R,   						//	VGA Red[9:0]
-// 		VGA_G,	 						//	VGA Green[9:0]
-// 		VGA_B   						//	VGA Blue[9:0]
-// 	);
-
-// 	input			CLOCK_50;				//	50 MHz
-// 	input   [9:0]   SW;
-// 	input   [3:0]   KEY;
-
-// 	// Declare your inputs and outputs here
-// 	// Do not change the following outputs
-// 	output			VGA_CLK;   				//	VGA Clock
-// 	output			VGA_HS;					//	VGA H_SYNC
-// 	output			VGA_VS;					//	VGA V_SYNC
-// 	output			VGA_BLANK_N;				//	VGA BLANK
-// 	output			VGA_SYNC_N;				//	VGA SYNC
-// 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
-// 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
-// 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-	
-// 	wire resetn;
-// 	assign resetn = KEY[0];
-	
-// 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
-// 	wire [2:0] colour;
-// 	wire [7:0] x;
-// 	wire [6:0] y;
-// 	wire writeEn;
-
-// 	// Create an Instance of a VGA controller - there can be only one!
-// 	// Define the number of colours as well as the initial background
-// 	// image file (.MIF) for the controller.
-// 	vga_adapter VGA(
-// 			.resetn(resetn),
-// 			.clock(CLOCK_50),
-// 			.colour(colour),
-// 			.x(x),
-// 			.y(y),
-// 			.plot(writeEn),
-// 			/* Signals for the DAC to drive the monitor. */
-// 			.VGA_R(VGA_R),
-// 			.VGA_G(VGA_G),
-// 			.VGA_B(VGA_B),
-// 			.VGA_HS(VGA_HS),
-// 			.VGA_VS(VGA_VS),
-// 			.VGA_BLANK(VGA_BLANK_N),
-// 			.VGA_SYNC(VGA_SYNC_N),
-// 			.VGA_CLK(VGA_CLK));
-// 		defparam VGA.RESOLUTION = "160x120";
-// 		defparam VGA.MONOCHROME = "FALSE";
-// 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-// 		defparam VGA.BACKGROUND_IMAGE = "chessgrid.mif";
-			
-// 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
-// 	// for the VGA controller, in addition to any other functionality your design may require.
-    
-//     // Instansiate datapath
-// 	// datapath d0(...);
-
-//     // Instansiate FSM control
-//     // control c0(...);
-    
-// endmodule
-
-
-module MainModule(SW, KEY, LEDR, CLOCK_50, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
     // SW[3:0] that specifies the address of intended move
-    input [3:0] SW;
-    // KEY0 clock, KEY1 reset, KEY2 confirm move
-    input [2:0] KEY;
+    // KEY[0] reset, KEY[1] confirm move
+	input			CLOCK_50;				//	50 MHz
+	input   [9:0]   SW;
+	input   [3:0]   KEY;
+
+    // LEDR[2] displays the end signal
     // LEDR[1:0] displays the outcome of the game
-	 
-	input CLOCK_50;
-	 
     output [2:0] LEDR;
-    output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-    wire [1:0] grid [2:0][2:0];;
-    MileStoneOne mso(
-        .resetn(KEY[1]),
+	// Declare your inputs and outputs here
+	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	wire resetn;
+    wire [1:0] grid [2:0][2:0];
+	assign resetn = KEY[0];
+
+
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour;
+	wire [7:0] x;
+	wire [6:0] y;
+	wire writeEn;
+
+    assign writeEn = 1'b1;
+    MileStoneTwo mst(
+        .resetn(KEY[0]),
         .clk(CLOCK_50),
-        .confirm(KEY[2]),
+        .confirm(KEY[1]),
         .address(SW[3:0]),
         .grid(grid[2:0][2:0]]),
         .winner(LEDR[1:0]),
+        .x_out(x),
+        .y_out(y),
+        .colour_out(colour),
 		.end_signal(LEDR[2])
     );
 
-    GridDecoder gd(grid[2:0][2:0], 
-    HEX5[6:0], 
-    HEX4[6:0], 
-    HEX3[6:0], 
-    HEX2[6:0], 
-    HEX1[6:0], 
-    HEX0[6:0]);
-	
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(resetn),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "chessgrid.mif";
+			
+	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+	// for the VGA controller, in addition to any other functionality your design may require.
+    
+    // Instansiate datapath
+	// datapath d0(...);
 
+    // Instansiate FSM control
+    // control c0(...);
+    
 endmodule
 
-module MileStoneOne(resetn, clk, confirm, address, grid, winner, end_signal);
+module MileStoneTwo(resetn, clk, confirm, address, grid, winner, x_out, y_out, colour_out, end_signal);
     input resetn;
     input clk;
     input confirm;
+    input [3:0] address;
+    output [7:0] x_out;
+    output [6:0] y_out;
+    output [2:0] colour_out;
     output [1:0] winner;
     output [1:0] grid [2:0][2:0];;
-    input [3:0] address;
     wire ld;
 	wire [1:0] value;
     output end_signal;
@@ -132,12 +121,21 @@ module MileStoneOne(resetn, clk, confirm, address, grid, winner, end_signal);
     .value(value));
 
     DataPathGrid dg(
-	 .resetn(resetn), 
+	.resetn(resetn), 
     .value(value), 
     .ld(ld), 
     .address(address),
-	 .grid(grid[2:0][2:0]),
+	.grid(grid[2:0][2:0]),
     .clk(clk));
+
+    ActualToDraw atd(
+        .grid(grid),
+        .x_out(x_out),
+        .y_out(y_out),
+        .colour_out(colour_out),
+        .clk(clk),
+        .resetn(resetn)
+    );
 endmodule
 module FSMControl(clk, resetn, confirm, end_sig, ld, value);
     input clk;
