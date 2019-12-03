e6668711dc6f66ee60ac41db0f14d801e655bdab
Yunfei Ouyang <yunfei.ouyang@mail.utoronto.ca>
2019-12-02 19:25:03 -0500

    added background mif file

--- MainModule.v
+++ MainModule.v
@@ -101,7 +101,7 @@ module MainModule
 		defparam VGA.RESOLUTION = "160x120";
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+		defparam VGA.BACKGROUND_IMAGE = "image.colour.mif";
 			
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
